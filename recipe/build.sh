350058cc0a9a5ec5acfa368b3ffe30f6fe7bf0c2
Rafael M Mudafort <rafmudaf@gmail.com>
2022-05-31 17:13:52 -0500

    Add FAST.Farm target to the build (#21)
    * Add more targets to the build
    
    * Use tarball source
    
    * Add source and build dir to CMake command
    
    * Disable C++ interface build
    
    * Roll the build number to 1
    
    * Test additional targets that support -h
    
    * Add OpenMP dependency for FAST.Farm

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,9 +4,12 @@ mkdir build
 cd build
 
 cmake \
+    -S ${SRC_DIR} \
+    -B . \
     -DDOUBLE_PRECISION=OFF \
     -DCMAKE_INSTALL_PREFIX=${PREFIX} \
     -DCMAKE_INSTALL_LIBDIR=lib \
-    ..
+    -DBUILD_FASTFARM=ON
+    # -DBUILD_OPENFAST_CPP_API=ON
 
 make -j"${CPU_COUNT}" install
