08dedf23e3f9ebed840f3b9cff93a74cdfae081c
Garrett Barter <garrett.barter@nrel.gov>
2023-01-08 04:25:32 -0700

    make runs out of memory, so trying ninja

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -12,24 +12,25 @@ cd build
 cmake ^
     -S %SRC_DIR% ^
     -B . ^
-    -G "MinGW Makefiles" ^
+    -GNinja ^
     -DCMAKE_BUILD_TYPE=RelWithDebInfo ^
     -DDOUBLE_PRECISION=OFF ^
     -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
     -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
     -DCMAKE_INSTALL_LIBDIR=lib ^
     -DBUILD_FASTFARM=ON ^
-    -DCMAKE_MAKE_PROGRAM=make ^
     -DCMAKE_C_COMPILER="C:/ProgramData/chocolatey/bin/gcc.exe" ^
     -DCMAKE_CXX_COMPILER="C:/ProgramData/chocolatey/bin/g++.exe" ^
     -DCMAKE_Fortran_COMPILER="C:/ProgramData/chocolatey/bin/gfortran.exe"
-REM    -DCMAKE_MAKE_PROGRAM="C:/ProgramData/chocolatey/bin/make.exe" ^
+
+REM THIS CONFIG WORKS BUT RUNS OUT OF MEMORY
+REM    -G "MinGW Makefiles" ^
+REM    -DCMAKE_MAKE_PROGRAM=make ^
+if errorlevel 1 exit /b 1
+
 REM    -DCMAKE_LINKER=lld-link               ^
 REM    -DCMAKE_NM=llvm-nm
 REM    -DBUILD_OPENFAST_CPP_API=ON
-
-if errorlevel 1 exit /b 1
-
 REM cmake -S . -B build  ^
 REM         -G "Ninja"                            ^
 REM    -G "MinGW Makefiles" ^
@@ -40,7 +41,8 @@ REM         -DCMAKE_LINKER=lld-link               ^
 REM         -DCMAKE_NM=llvm-nm
 	
 REM nmake -j %CPU_COUNT% install
-make -j %CPU_COUNT% install
+REM make -j %CPU_COUNT% install
+ninja -j %CPU_COUNT% install
 
 REM cmake --target install
 if errorlevel 1 exit /b 1
