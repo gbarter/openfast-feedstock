a8731bb33a589194d3057239ecb7e0e6c33b95a3
Garrett Barter <garrett.barter@nrel.gov>
2023-01-09 08:29:57 -0700

    Prior build worked!  Trying to simplify and cleanup a bit

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -14,7 +14,7 @@ cmake ^
     -S %SRC_DIR% ^
     -B . ^
     -G "Unix Makefiles" ^
-    -DCMAKE_MAKE_PROGRAM="C:/ProgramData/chocolatey/bin/make.exe" ^
+    -DCMAKE_MAKE_PROGRAM="%ProgramData%/chocolatey/bin/make.exe" ^
     -DCMAKE_FIND_LIBRARY_SUFFIXES=".a" ^
     -DCMAKE_C_FLAGS="-O1 -static" ^
     -DCMAKE_CXX_FLAGS="-O1 -static" ^
@@ -25,9 +25,9 @@ cmake ^
     -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
     -DCMAKE_INSTALL_LIBDIR=lib ^
     -DBUILD_FASTFARM=ON ^
-    -DCMAKE_C_COMPILER="C:/ProgramData/chocolatey/bin/gcc.exe" ^
-    -DCMAKE_CXX_COMPILER="C:/ProgramData/chocolatey/bin/g++.exe" ^
-    -DCMAKE_Fortran_COMPILER="C:/ProgramData/chocolatey/bin/gfortran.exe"
+    -DCMAKE_C_COMPILER="%ProgramData%/chocolatey/bin/gcc.exe" ^
+    -DCMAKE_CXX_COMPILER="%ProgramData%/chocolatey/bin/g++.exe" ^
+    -DCMAKE_Fortran_COMPILER="%ProgramData%/chocolatey/bin/gfortran.exe"
 
 REM This config almost works with mingw through choco (v8 on Azure, v12 locally)
 REM Has linking problems in that the executables don't run after the fact (choco bin-dir not in PATH)
@@ -44,28 +44,12 @@ REM Builds only with shared libs locally, but nothing runs ("jom" added through 
 REM     -DBUILD_SHARED_LIBS=ON ^
 REM         -G "NMake Makefiles JOM"              ^
 
-REM Does not link nicely with MinGW on Azure
-REM     -DBUILD_SHARED_LIBS=ON ^
-
 
 if errorlevel 1 exit /b 1
-
 REM    -DBUILD_OPENFAST_CPP_API=ON
-REM         -DCMAKE_C_COMPILER=clang-cl           ^
-REM         -DCMAKE_CXX_COMPILER=clang-cl         ^
-REM         -DCMAKE_LINKER=lld-link               ^
-REM         -DCMAKE_NM=llvm-nm
 	
 cmake --build . -j 1
-
-REM Runs out of memory
-REM make -j %CPU_COUNT% install
-
-REM See ninja error above
-REM ninja -j %CPU_COUNT% install
-REM make
 if errorlevel 1 exit /b 1
 
-REM make install
 cmake --install .
 if errorlevel 1 exit /b 1
