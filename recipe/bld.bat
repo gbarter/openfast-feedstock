16bfb71fab4d2f0d9f36c2809f5ee57402755496
Derek Slaughter <deslaughter@gmail.com>
2023-12-10 11:41:59 -0500

    Install baskit

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,7 +3,7 @@
 curl.exe --output webimage.exe --url https://registrationcenter-download.intel.com/akdlm/IRC_NAS/62641e01-1e8d-4ace-91d6-ae03f7f8a71f/w_BaseKit_p_2024.0.0.49563_offline.exe
 start /b /wait webimage.exe -s -x -f webimage_extracted --log extract.log
 del webimage.exe
-webimage_extracted\bootstrapper.exe -s --action install --components=intel.oneapi.win.mkl.devel --eula=accept -p=NEED_VS2017_INTEGRATION=0 -p=NEED_VS2019_INTEGRATION=0 -p=NEED_VS2022_INTEGRATION=0 --log-dir=.
+webimage_extracted\bootstrapper.exe -s --action install --components=intel.oneapi.win.basekit.product:intel.oneapi.win.mkl.devel --eula=accept -p=NEED_VS2017_INTEGRATION=0 -p=NEED_VS2019_INTEGRATION=0 -p=NEED_VS2022_INTEGRATION=0 --log-dir=.
 
 curl.exe --output webimage.exe --url https://registrationcenter-download.intel.com/akdlm/IRC_NAS/5b36181e-4974-4733-91c7-0c10c54900a5/w_HPCKit_p_2024.0.0.49588_offline.exe
 start /b /wait webimage.exe -s -x -f webimage_extracted --log extract.log
