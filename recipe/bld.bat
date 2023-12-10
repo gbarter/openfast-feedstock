e5d8694fe4df9887007300dfb390ccdad2eb943e
Derek Slaughter <deslaughter@gmail.com>
2023-12-10 10:02:44 -0500

    Remove bad line from bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,8 +3,7 @@
 curl.exe --output webimage.exe --url https://registrationcenter-download.intel.com/akdlm/IRC_NAS/5b36181e-4974-4733-91c7-0c10c54900a5/w_HPCKit_p_2024.0.0.49588_offline.exe
 start /b /wait webimage.exe -s -x -f webimage_extracted --log extract.log
 del webimage.exe
-webimage_extracted\bootstrapper.exe -s --action install --components=intel.oneapi.win.ifort-compiler --eula=accept -p=NEED_VS2017_INTEGRATION=0 -p=NEED_VS2019_INTEGRATION=0 -p=NEED_VS2022_INTEGRATION=0 --log-dir=.
-echo ONEAPI_ROOT=C:\Program Files (x86)\Intel\oneAPI >> %GITHUB_ENV%
+webimage_extracted\bootstrapper.exe -s --action install --components=intel.oneapi.win.ifort-compiler --eula=accept -p=NEED_VS2017_INTEGRATION=0 -p=NEED_VS2019_INTEGRATION=0 -p=NEED_VS2022_INTEGRATION=1 --log-dir=.
 
 @call "C:\Program Files (x86)\Intel\oneAPI\setvars-vcvarsall.bat"
 
@@ -23,8 +22,6 @@ cmake ^
     -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
     -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
     -DBUILD_FASTFARM=ON ^
-    -DCMAKE_C_COMPILER=cl.exe ^
-    -DCMAKE_CXX_COMPILER=cl.exe ^
     -DCMAKE_Fortran_COMPILER=ifort
 if errorlevel 1 exit /b 1
 	
