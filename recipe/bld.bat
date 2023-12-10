d3527a533a2407f0ce527f0affeab77e079fd907
Derek Slaughter <deslaughter@gmail.com>
2023-12-10 10:39:19 -0500

    Skip VS2022 integration

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,18 +3,16 @@
 curl.exe --output webimage.exe --url https://registrationcenter-download.intel.com/akdlm/IRC_NAS/5b36181e-4974-4733-91c7-0c10c54900a5/w_HPCKit_p_2024.0.0.49588_offline.exe
 start /b /wait webimage.exe -s -x -f webimage_extracted --log extract.log
 del webimage.exe
-webimage_extracted\bootstrapper.exe -s --action install --components=intel.oneapi.win.ifort-compiler --eula=accept -p=NEED_VS2017_INTEGRATION=0 -p=NEED_VS2019_INTEGRATION=0 -p=NEED_VS2022_INTEGRATION=1 --log-dir=.
+webimage_extracted\bootstrapper.exe -s --action install --components=intel.oneapi.win.ifort-compiler --eula=accept -p=NEED_VS2017_INTEGRATION=0 -p=NEED_VS2019_INTEGRATION=0 -p=NEED_VS2022_INTEGRATION=0 --log-dir=.
 
-@call "C:\Program Files (x86)\Intel\oneAPI\setvars-vcvarsall.bat"
+@call "C:\Program Files (x86)\Intel\oneAPI\setvars.bat"
 
 mkdir build
-cd build
-
 set LDFLAGS="-static"
 
 cmake ^
     -S %SRC_DIR% ^
-    -B . ^
+    -B build ^
     -G "Ninja" ^
     -DCMAKE_BUILD_TYPE="Release" ^
     -DDOUBLE_PRECISION=OFF ^
