fa95b1044de842b4b373f9a7fc8915863cca29ae
Garrett Barter <garrett.barter@nrel.gov>
2024-10-22 11:36:26 -0600

    updates

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,20 +3,21 @@
 mkdir build
 
 :: set compilers to gcc
-set "CC=gcc"
-set "CXX=g++"
-set "FC=gfortran"
+set "CC=gcc.exe"
+set "CXX=g++.exe"
+set "FC=gfortran.exe"
+::     -G "Ninja" ^
+::    -DCMAKE_Fortran_COMPILER=%FC% ^
+::    -DCMAKE_C_COMPILER=%CC% ^
+::    -DCMAKE_CXX_COMPILER=%CXX% ^
 
 
 cmake ^
     -S %SRC_DIR% ^
     -B build ^
-    -G "Ninja" ^
+    -G "Unix Makefiles" ^
     -DCMAKE_BUILD_TYPE="Release" ^
     -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
-    -DCMAKE_Fortran_COMPILER=%FC% ^
-    -DCMAKE_C_COMPILER=%CC% ^
-    -DCMAKE_CXX_COMPILER=%CXX% ^
     -DCMAKE_MSVC_RUNTIME_LIBRARY=MultiThreaded ^
     -DBLA_STATIC=ON ^
     -DDOUBLE_PRECISION=OFF ^
