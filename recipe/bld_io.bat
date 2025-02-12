15a52d66c2aed1e664823d81983adcd3c539ce9d
Garrett Barter <garrett.barter@nrel.gov>
2025-02-12 11:59:01 -0700

    moving to scripts

---
+++ recipe/bld_io.bat
@@ -0,0 +1,4 @@
+@echo on
+
+%PYTHON% -m pip install --no-deps --no-build-isolation -vv ./openfast_io
+
---
+++ recipe/build_io.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+"${PYTHON}" -m pip install --no-deps --no-build-isolation -vv ./openfast_io
+
